6f479277db4e41fdbf9a2cbeb41de6070bef8029
Minyong#2020.06.14 <ricky23@yonsei.ac.kr>
2020-06-14 19:10:50 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.listen(4000);
+const POST = 4000;
+
+function handleHome(req, res) {
+    console.log(req);
+    res.send('Hello from home');
+}
+app.get("/", handleHome);
+
+function handleProfile(req, res) {
+    res.send("You are on my Profile");
+}
+app.get("/profile", handleProfile);
+
+handleListening = () => {
+    console.log(`Listening Start on : http://localhost:${POST}`);
+}
+app.listen(POST, handleListening);
