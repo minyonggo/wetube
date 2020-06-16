0ee2f79ab3fffe8f991a07c74bc8a5bb6a5fed93
Minyong#2020.06.16 <ricky23@yonsei.ac.kr>
2020-06-16 11:26:57 +0900

    #2.7 Express Core: Middlewares part.2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 //const express = require('express');
+import morgan from "morgan";
 const app = express();
 
 const PORT = 4000;
@@ -18,7 +22,13 @@ const handleListening = () => {
     console.log(`Listening Start on : http://localhost:${PORT}`);
 }
 
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
