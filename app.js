3bf5b3067808a89a39ec23ac2574b1d9550f134a
Minyong#2020.06.16 <ricky23@yonsei.ac.kr>
2020-06-16 19:48:14 +0900

    #2.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -10,12 +10,19 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev"));
+
+
+app.use((req, res, next) => {
+    res.locals.siteName = "WeTube";
+    res.locals.routes = routes;
+    next();
+});
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
