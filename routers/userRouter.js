946784aa16badbeb6af6e8840fab455cd932a7d7
Minyong#2020.06.16 <ricky23@yonsei.ac.kr>
2020-06-16 16:15:28 +0900

    #2.13 Installing Pug

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,5 +9,4 @@ userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail, userDetail);
 
-
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import routes from '../routes';
-import { vidoes, upload, videoDetail, editVideo, deleteVideo } from '../controllers/videoController';
+import { videos, upload, videoDetail, editVideo, deleteVideo } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.home, vidoes);
+videoRouter.get(routes.home, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
