0109cd68a751a4573d886638de71abef90897e9c
Minyong#2020.06.16 <ricky23@yonsei.ac.kr>
2020-06-16 11:59:45 +0900

    #2.8 Express Core : Router

--- app.js
+++ app.js
@@ -2,25 +2,18 @@ import express from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-//const express = require('express');
 import morgan from "morgan";
+import {userRouter} from "./router";
+
 const app = express();
 
-const PORT = 4000;
-
-const betweenHome = (req, res, next) => {
-    console.log("Between request and router");
-    next();
-}
 const handleHome = (req, res) => {
     res.send('Hello from home!');
 }
 const handleProfile = (req, res) => {
     res.send("You are on my Profile!");
 }
-const handleListening = () => {
-    console.log(`Listening Start on : http://localhost:${PORT}`);
-}
+
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -31,4 +24,6 @@ app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
