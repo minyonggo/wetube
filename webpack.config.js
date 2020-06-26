032427c90a7b8e871b9e28cc19c1320970800374
Minyong#2020.06.17 <ricky23@yonsei.ac.kr>
2020-06-26 15:52:33 +0900

    #4.2 ES6 with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,51 @@
 const path = require("path");
+const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
+const MODE = process.env.WEBPACK_ENV;
+
 const config = {
+  mode: MODE,
   entry: ENTRY_FILE,
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => {
+                return [autoprefixer({ Browserslist: "cover 99.5%" })];
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ]),
+      },
+    ],
+  },
+  plugins: [new ExtractCSS("styles.css")],
 };
 
 module.exports = config;
